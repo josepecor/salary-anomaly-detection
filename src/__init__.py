0171d3f6ccf3f3b7dffad830e0b2431df3ebb23f
Josep Esparrell <josepecor@gmail.com>
2025-12-30 20:00:25 +0100

    feat: add custom modue to model training and explicability

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,9 @@ __version__ = '0.1.0'
 
 # Importar funciones principales para acceso directo
 from .data_management import get_categorical_number_columns, delete_columns
+from .model_trainer import ModelTrainer
 __all__ = [
-    'get_categorical_number_columnsdata',
+    'get_categorical_number_columns',
     'delete_columns',
+    'ModelTrainer'
 ]
